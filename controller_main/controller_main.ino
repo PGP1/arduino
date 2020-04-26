5dda29090c34a275e8da1d9050c498a672461b45
nugyn <s3429599@student.rmit.edu.au>
2020-04-26 15:05:29 +1000

    removed redundant sensor_main.ino, and combined into controller_main.ino

--- controller_main/controller_main.ino
+++ controller_main/controller_main.ino
@@ -13,10 +13,11 @@ void setup() {
   // start the serial console
   Serial.begin(9600);
   
+  //mock data generation
   randomSeed(analogRead(0));
   
+  // set up pin to communicate to sensors
   pinMode(led, OUTPUT);
-  //all other "init" code goes here as well
 }
 
 //take a ph level reading
@@ -56,13 +57,10 @@ int water_level() {
  return resval;
 }
 
-String receiver() {
+String parsePayload() {
   String data;
   if (Serial.available() > 0) {
     data = Serial.readStringUntil('\n');
-    //Serial.print("Arduino [DEBUG]: received from Raspberry pi:  ");
-    //Serial.println(data);
-    //Serial.flush();
   }
   return data;
 }
@@ -70,41 +68,68 @@ String receiver() {
 int light(int* frequency) {
   analogWrite(led, frequency);
 }
-  
-//the infinite loop of sensor reading
-void loop() { 
-String payload;
 
-//listen to topic
-if(Serial.available()) {
-  const size_t capacity = JSON_ARRAY_SIZE(1) + 2*JSON_OBJECT_SIZE(3) + 40;
+void mockData() {
+  //will be sent to topic: 37e781d9d23260a2a66b7fe7b638e314df/sensor
+  Serial.println(); 
+  //clear buffer and init json
+  
+  const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5);
   DynamicJsonDocument doc(capacity);
-  payload = receiver();
-  DeserializationError err = deserializeJson(doc, payload);
-  
-  int data_0_fan = doc["controller"]["fan"].as<int>();
-  int data_0_light = doc["controller"]["light"].as<int>();
-  int data_0_pump = doc["controller"]["pump"].as<int>();
 
-  if(data_0_light >= 0) {
-    Serial.println("Arduino [DEBUG]:{\"Success\":\"True\"}");
-    light(data_0_light);    
-  }
-  else if(data_0_pump == 1) {
+  doc["device"] = device_uuid;
+  doc["time"] = "20-18-04T11:24:36Z";
+
+  JsonArray data = doc.createNestedArray("data");
+
+  JsonObject data_0 = data.createNestedObject();
+  data_0["temp"] = temp();
+  data_0["humidity"] = humidity();
+  data_0["water"] = 330;
+  data_0["ph"] = ph();
+  data_0["ldr"] = 485;
+
+  serializeJson(doc, Serial);
+
+  delay(8000);
+}
+
+void receiver(){
+  String payload;
+  //listen to topic
+  if(Serial.available()) {
+    const size_t capacity = JSON_ARRAY_SIZE(1) + 2*JSON_OBJECT_SIZE(3) + 40;
+    DynamicJsonDocument doc(capacity);
+    payload = parsePayload();
+    DeserializationError err = deserializeJson(doc, payload);
     
-  }
-  else if(data_0_fan == 1) {
- 
-  }
-  else {
-    Serial.print("Arduino [DEBUG]: deserializeJson() returned ");
-    Serial.println(err.c_str());
+    int data_0_fan = doc["controller"]["fan"].as<int>();
+    int data_0_light = doc["controller"]["light"].as<int>();
+    int data_0_pump = doc["controller"]["pump"].as<int>();
 
-    while(Serial.available() > 0) {
-      Serial.read();
+    if(data_0_light >= 0) {
+      Serial.println("Arduino [DEBUG]:{\"Success\":\"True\"}");
+      light(data_0_light);    
+    }
+    else if(data_0_pump == 1) {
+      
+    }
+    else if(data_0_fan == 1) {
+  
+    }
+    else {
+      Serial.print("Arduino [DEBUG]: deserializeJson() returned ");
+      Serial.println(err.c_str());
+
+      while(Serial.available() > 0) {
+        Serial.read();
+      }
     }
   }
 }
 
-delay(8000);
+//the infinite loop of sensor reading
+void loop() { 
+  receiver();
+  delay(8000);
 }
