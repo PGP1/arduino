5cd07dde5592e1ed7bddf362c59b3469e03c15af
Duy Nguyen <41281749+nugyn@users.noreply.github.com>
2020-05-03 14:34:46 +1000

    [US1.5] Sensors (#11)
    * [US1.5] Sensors: ph sensor
    
    * [US1.5]Sensors: completed implementation of ph, waterlevel, temp
    
    * [US1.5]Sensors: saved brightness of light, to send back to the user, and some refactoring
    
    * [US1.5]Sensors: refactor
    
    * implemented ph, waterlevel, temp & humidity, sensor data push
    
    * refactor

--- controller_main/controller_main.ino
+++ controller_main/controller_main.ino
@@ -1,125 +1,203 @@
 #include <ArduinoJson.h>
+#include "DHT.h"
+#define PHANALOGPIN A0
+#define DHTDIGITALPIN 7
+#define DHTTYPE DHT11
+#define WATERLEVELANALOGPIN A1
+#define LEDPOWER 11
+#define WATERLEVELPOWER 5
 
-int resval = 0;  // holds the value
-int respin_A5 = A5; // sensor pin used
-char device_uuid [8] = "ef3zf67";
+DHT dht(DHTDIGITALPIN, DHTTYPE);
 
-int led = 11;
+unsigned long int avgValue;
+float b;
+int buf[10], temporary;
+
+int waterLevelResval = 0; // holds the value
+char deviceUUID[8] = "ef3zf67";
+
 int brightness = 0;
 int fadeAmount = 5;
 
-void setup() { 
- 
-  // start the serial console
+void setup()
+{
+
+  // start the serial connnection
   Serial.begin(9600);
-  
+
   //mock data generation
   randomSeed(analogRead(0));
-  
-  // set up pin to communicate to sensors
-  pinMode(led, OUTPUT);
+
+  // setup pin to communicate to sensors
+  pinMode(LEDPOWER, OUTPUT);
+  pinMode(WATERLEVELPOWER, OUTPUT);
+	
+  // Supply power to water level sensor
+  digitalWrite(WATERLEVELPOWER, HIGH);
+
+  // setup dht sensor
+  dht.begin();
 }
 
 //take a ph level reading
-float ph() {
-  //resval = digitRead(respin_A6);
-  //return resval
-  float ph = random(5.62,6.11);
-  return ph;
+float ph()
+{
+
+  for (int i = 0; i < 10; i++)
+  {
+    buf[i] = analogRead(PHANALOGPIN);
+    delay(10);
+  }
+
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = i + 1; j < 10; j++)
+    {
+      if (buf[i] > buf[j])
+      {
+        temporary = buf[i];
+        buf[i] = buf[j];
+        buf[j] = temporary;
+      }
+    }
+  }
+  avgValue = 0;
+
+  for (int i = 2; i < 8; i++)
+  {
+    avgValue += buf[i];
+  }
+
+  float phValue = (float)avgValue * 5.0 / 1024 / 6; //convert the analog into millivolt
+  phValue = 3.5 * phValue;                          //convert the millivolt into pH value
+  
+  // Mock Data
+  // resval = digitRead(respin_A6);
+  // return resval
+  // float ph = random(5.62,6.11);
+  // return ph;
+
+  return phValue;
 }
 
 // take a reading from the temp sensor
-float temp() {
-  float temp = random(20.01,28.99);
+float temp()
+{
+  // Mock Data
+  // float temp = random(20.01,28.99);
+  // return temp;
+  float temp = dht.readTemperature();
   return temp;
 }
 
 //take a reading from the humidity sensor
-int humidity() { 
-  int humidity = random(45,69);
-  //if possible then humidity reported separatetly with this method
+int humidity()
+{
+  // Mock Data
+  // int humidity = random(45,69);
+  // if possible then humidity repeated separatetly with this method
+  // return humidity;
+  float humidity = dht.readHumidity();
   return humidity;
 }
 
 //take a reading from the water-level sensor
-int water_level() {
- //take reading from analog pin and store it to resval variable
- resval = analogRead(respin_A5);
- if (resval<=100){
-   Serial.println("Water Level: Empty");
- } else if (resval>100 && resval<=300){
-   Serial.println("Water Level: Low");
- } else if (resval>300 && resval<=330){
-   Serial.println("Water Level: Medium");
- } else if (resval>330){
-   Serial.println("Water Level: High");
- }
- return resval;
+int waterLevel()
+{
+  //take reading from analog pin and store it to resval variable
+  waterLevelResval = analogRead(WATERLEVELANALOGPIN);
+  if (waterLevelResval <= 100)
+  {
+    Serial.println("Water Level: Empty");
+  }
+  else if (waterLevelResval > 100 && waterLevelResval <= 300)
+  {
+    Serial.println("Water Level: Low");
+  }
+  else if (waterLevelResval > 300 && waterLevelResval <= 330)
+  {
+    Serial.println("Water Level: Medium");
+  }
+  else if (waterLevelResval > 330)
+  {
+    Serial.println("Water Level: High");
+  }
+  return waterLevelResval;
 }
 
-String parsePayload() {
+String parsePayload()
+{
   String data;
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0)
+  {
     data = Serial.readStringUntil('\n');
   }
   return data;
 }
 
-int light(int* frequency) {
-  analogWrite(led, frequency);
+int changeLightBrightness(int *frequency)
+{
+  analogWrite(LEDPOWER, frequency);
+  brightness = frequency;
+
 }
 
-void mockData() {
-  //will be sent to topic: 37e781d9d23260a2a66b7fe7b638e314df/sensor
-  Serial.println(); 
+void sensorData()
+{
+  Serial.println();
   //clear buffer and init json
-  
+
   const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5);
   DynamicJsonDocument doc(capacity);
 
-  doc["device"] = device_uuid;
+  doc["device"] = deviceUUID;
   JsonArray data = doc.createNestedArray("data");
 
   JsonObject data_0 = data.createNestedObject();
   data_0["temp"] = temp();
   data_0["humidity"] = humidity();
-  data_0["water"] = 330;
+  data_0["water"] = waterLevel();
   data_0["ph"] = ph();
-  data_0["ldr"] = 485;
+  data_0["ldr"] = brightness;
 
   serializeJson(doc, Serial);
 
   delay(8000);
 }
 
-void receiver(){
+void receiver()
+{
   String payload;
   //listen to topic
-  if(Serial.available()) {
-    const size_t capacity = JSON_ARRAY_SIZE(1) + 2*JSON_OBJECT_SIZE(3) + 40;
+  if (Serial.available())
+  {
+    const size_t capacity = JSON_ARRAY_SIZE(1) + 2 * JSON_OBJECT_SIZE(3) + 40;
     DynamicJsonDocument doc(capacity);
     payload = parsePayload();
     DeserializationError err = deserializeJson(doc, payload);
-    
+
     int data_0_fan = doc["controller"]["fan"].as<int>();
     int data_0_light = doc["controller"]["light"].as<int>();
     int data_0_pump = doc["controller"]["pump"].as<int>();
 
-    if(data_0_light >= 0) {
+    if (data_0_light >= 0)
+    {
       Serial.println("Arduino [DEBUG]:{\"Success\":\"True\"}");
-      light(data_0_light);    
+      changeLightBrightness(data_0_light);
     }
-    else if(data_0_pump == 1) {
-      
+    else if (data_0_pump == 1)
+    {
     }
-    else if(data_0_fan == 1) {
-  
+    else if (data_0_fan == 1)
+    {
     }
-    else {
+    else
+    {
       Serial.print("Arduino [DEBUG]: deserializeJson() returned ");
       Serial.println(err.c_str());
 
-      while(Serial.available() > 0) {
+      while (Serial.available() > 0)
+      {
         Serial.read();
       }
     }
@@ -127,7 +205,8 @@ void receiver(){
 }
 
 //the infinite loop of sensor reading
-void loop() { 
+void loop()
+{
   receiver();
-  delay(8000);
+  sensorData();
 }
